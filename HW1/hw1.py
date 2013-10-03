7ba9f1b606e0302b890a95ef49e9ada83923e75a
ngutro25 <ngutro25@evergreen.edu>
2013-10-03 14:45:04 -0700

    equestions

--- HW1/hw1.py
+++ HW1/hw1.py
@@ -18,7 +18,22 @@ import math                     # makes the math.sqrt function available
 
 print "Problem 1: Roots - solution follows:"
 
-# ... write your code and comments here (and remove this line)
+## Compute and print both roots of the quadratic equation (x*x)-5.86 x+ 8.5408
+## 
+
+# Define Variables
+a = 1
+b = -5.86
+c = 8.5408
+
+# rootadd
+rootadd = (-(b) + ((b**2) - (4 * a * c)) ) / (2* a)
+
+# rootsubt
+rootsubt = (-(b) - ((b**2) - (4 * a * c)) ) / (2* a)
+
+print("The add-quadratic: " +str(rootadd))
+print("The sub-quadratic: " +str(rootsubt)) 
 
 
 ###
@@ -27,7 +42,15 @@ print "Problem 1: Roots - solution follows:"
 
 print "Problem 2: Importing -  solution follows:"
 
-# ... write your code and comments here (and remove this line)
+# import file and variables
+from hw1_test import a,b,c,d,e,f
+# print out the result abcdef
+print("a = " +str(a))
+print("b = " +str(b))
+print("c = " +str(c))
+print("d = " +str(d))
+print("e = " +str(e))
+print("f = " +str(f))
 
 
 ###
@@ -38,11 +61,12 @@ print "Problem 3 solution follows:"
 
 # ... write your code and comments here (and remove this line)
 
+print "The Boolean Expression: ((a and b) or (not c) and not (d or e or f))"
 
-
+print((a and b) or (not c) and not (d or e or f))        
 
 ###
-### Collaboration
+### Micah, Jason, Alex 
 ###
 
 # ... List your collaborators here, as a comment (on a line starting with "#").
